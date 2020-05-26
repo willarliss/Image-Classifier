caf7d4244ccc61030fed3fb6a1d1f4c7ca0de1f8
Will <65086773+willarliss@users.noreply.github.com>
2020-05-26 09:02:52 -0400

    Update img_save.py

--- img_save.py
+++ img_save.py
@@ -3,25 +3,28 @@ from PIL import Image
 import os
     
 path = os.getcwd()+'\\images'
+files_B = glob('./downloads/borzoi dog - Google Search_files/*')
+files_D = glob('./downloads/dachshund dog - Google Search_files/*')
+
 count = 1
 
-files_B = glob('./downloads/borzoi dog - Google Search_files/*')
 for file in files_B:
 
     try:
         img = Image.open(file)
         img.save(os.path.join(path, f'borzoi{count}.jpg'))
         count += 1
+    
     except:
         pass
       
-files_D = glob('./downloads/dachshund dog - Google Search_files/*')
 for file in files_D:
 
     try:
         img = Image.open(file)
         img.save(os.path.join(path, f'dachshund{count}.jpg'))
         count += 1
+    
     except:
         pass
     
